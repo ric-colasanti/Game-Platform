c9efe641ff62ef1ff41693a8ebe732c630e5ca9e
Dr Ric Colasanti <rcolasanti@bournemouth.ac.uk>
2020-02-25 12:42:01 +0000

    add floor int conversion to the drawImage

--- js/background.js
+++ js/background.js
@@ -9,11 +9,11 @@ function Background(canvas,context,img,offset,vx){
 
 Background.prototype.draw =  function(delta){
     this.xpos = this.xpos - delta * this.vx ;
-    this.context.drawImage(this.img,this.xpos,this.yOffset);
+    this.context.drawImage(this.img,Math.floor(this.xpos),Math.floor(this.yOffset));
     var imgEdge = this.xpos+this.img.width
     xOffset = imgEdge-this.canvas.width
     if(xOffset<0){
-        this.context.drawImage(this.img,imgEdge,this.yOffset);
+        this.context.drawImage(this.img,Math.floor(imgEdge),Math.floor(this.yOffset));
     }
     if(imgEdge<=0){
         this.xpos=imgEdge;
--- js/tile.js
+++ js/tile.js
@@ -8,8 +8,8 @@ function Tile(canvas,context,img,offset,xpos,vx){
 }
 
 Tile.prototype.draw =  function(delta){
-    this.xpos = Math.floor(this.xpos - delta * this.vx) ;
+    this.xpos = this.xpos - delta * this.vx ;
     if((this.xpos>=-this.img.width)&&(this.xpos<=this.canvas.width)){
-        this.context.drawImage(this.img,this.xpos,this.yOffset);
+        this.context.drawImage(this.img,Math.floor(this.xpos),Math.floor(this.yOffset));
     }
 };
