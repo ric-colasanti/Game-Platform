bceeeb7b4f3423340d23f2c9fbe4fcdf459e2bfa
Dr Ric Colasanti <rcolasanti@bournemouth.ac.uk>
2020-02-25 09:37:23 +0000

    add platform tiles.. problems with stuttering

---
+++ js/tile.js
@@ -0,0 +1,15 @@
+function Tile(canvas,context,img,offset,xpos,vx){
+    this.canvas = canvas;
+    this.context = context;
+    this.img = img;
+    this.yOffset = offset;
+    this.xpos = xpos; 
+    this.vx = vx  
+}
+
+Tile.prototype.draw =  function(delta){
+    this.xpos = Math.floor(this.xpos - delta * this.vx) ;
+    if((this.xpos>=-this.img.width)&&(this.xpos<=this.canvas.width)){
+        this.context.drawImage(this.img,this.xpos,this.yOffset);
+    }
+};
